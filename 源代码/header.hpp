8e84bd97c87a1dc88b60b699a81b21bc3b9bc5eb
AnOrange-599 <07232227@cumt.edu.cn>
2025-02-28 18:00:08 +0800

    Add files via upload

---
+++ 源代码/header.hpp
@@ -0,0 +1,5 @@
+#ifndef HEADER_HPP
+#define HEADER_HPP
+int main1();
+int main2();
+#endif
---
+++ 源代码/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "header.hpp"
+#include "processing.hpp"
+using namespace std;
+int main() {
+	cout << "输入1或2选择需要的功能\n输入\t1\t角度处理\n输入\t2\t导线测量计算\n";
+	int a = 0;
+	cin >> a;
+	if (a == 1)
+		main1();
+	else if (a == 2)
+		main2();
+	else
+		cout << "重输" << endl;
+	
+	system("pause");
+	return 0;
+}
